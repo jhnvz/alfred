7e573b88b2939a6dbada9613eb101c07a5ddacd9
Johan van Zonneveld <johan@vzonneveld.nl>
2014-04-07 23:20:00 +0200

    Better token

--- spec/support/rails.rb
+++ spec/support/rails.rb
@@ -3,10 +3,10 @@ ENV['RAILS_ENV'] = 'test'
 module AlfredRailsTest
   class Application < Rails::Application
     config.active_support.deprecation = :log
-    config.secret_token = 'existing secret token'
+    config.secret_token = '8f62080da9dd946a6555635c46fbebfb'
 
     if Rails::VERSION::STRING >= "4.0.0"
-      config.secret_key_base = 'new secret key base'
+      config.secret_key_base = '8f62080da9dd946a6555635c46fbebfb'
     end
   end
 end
