81af478ccd1da91e6a670a8765fade83def4c844
Johan van Zonneveld <johan@vzonneveld.nl>
2014-04-07 22:42:42 +0200

    Add secret token

--- spec/support/rails.rb
+++ spec/support/rails.rb
@@ -3,9 +3,9 @@ ENV['RAILS_ENV'] = 'test'
 module AlfredRailsTest
   class Application < Rails::Application
     config.active_support.deprecation = :log
+    config.secret_token = 'existing secret token'
 
     if Rails::VERSION::STRING >= "4.0.0"
-      config.secret_token    = 'existing secret token'
       config.secret_key_base = 'new secret key base'
     end
   end
