f4bd4fec809b1b96aa654d4cb02059b6f3f547fd
Johan van Zonneveld <johan@vzonneveld.nl>
2014-04-11 20:03:43 +0200

    More unit tests for Alfred

--- spec/alfred_spec.rb
+++ spec/alfred_spec.rb
@@ -2,6 +2,35 @@ require 'spec_helper'
 
 describe Alfred do
 
+  describe '.configuration' do
+
+    it 'should return new or existing instance of configuration' do
+      Alfred.configuration.mock_with :rr
+      Alfred.configuration.mock_with.should == :rr
+    end
+
+  end
+
+  describe '.config' do
+
+    it 'should yield configuration instance' do
+      Alfred.configure do |c|
+        c.mock_with :rr
+      end
+      Alfred.configuration.mock_with.should == :rr
+    end
+
+  end
+
+  describe '.registry' do
+
+    it 'should return new or existing instance of registry' do
+      Alfred.registry.register('id', 'item')
+      Alfred.registry['id'].should == ['item']
+    end
+
+  end
+
   describe '.include_modules!' do
 
     it 'should include configured modules in Request' do
